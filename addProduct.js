ffe05c2d6f89e7dc703c7dc466e52951d556183a
agamitsudo <agamitsudo@gmail.com>
2016-02-17 18:05:03 +0100

    add product

--- addProduct.js
+++ addProduct.js
@@ -4,34 +4,34 @@
 function addProduct(section)
 {
 	 clearSection(section);
-         $("#" + section).append("<div id='loggin'><form>User name:<br><input type='text' id='username'><br>User password:<br><input type='password' id='psw'></form></div><br><br>");
-         $("#" + section).append("<div id='data'><form>Bar code:<br><input type='text' id='barcode'><br>Product name:<br><input type='text' id='productname'><br><input id='add' type='button' value='add product'></form></div>");
+         $("#" + section).append("<div id='logging'><form>User name:<br><input type='text' id='z1'><br>User password:<br><input type='password' id='z2'></form></div><br><br>");
+         $("#" + section).append("<div id='data'><form>Bar code:<br><input type='text' id='z3'><br>Product name:<br><input type='text' id='z4'><br><input id='add' type='button' value='add product'></form></div><div id='result'></di");
 
-
-          $('#loggin').hide().fadeIn(500);
+          $('#logging').hide().fadeIn(500);
           $('#data').hide().fadeIn(1000);
 
           $('#add').click(function(){
- 		
-	      var foodfactObj = new Object();
 
-	      foodfactObj.code = $('#barcode').val();
-	      foodfactObj.product_name = $('#productname').val();
-	      foodfactObj.user_id = $('#username').val()
-	      foodfactObj.password = $('#psw').val();
-	      
-	      $.ajax({
-    			type: "POST",
-    			headers: {
-      			  'Accept': 'application/json',
-       			  'Content-Type': 'text/plain'
-  		},
-   		dataType: "json",
-   		url: "http://world.openfoodfacts.org/cgi/product_jqm2.pl",
-   		data: JSON.stringify(foodfactObj),
-    		success: function (data) {
-        		alert(JSON.stringify(data));
-    		}
-	      });   
+	      var url = "http://world.openfoodfacts.org/cgi/product_jqm2.pl?";
+	      var dt = 'user_id=' + $('#z1').val();
+
+	      dt += '&password=' + $('#z2').val();
+              dt +=  '&code=' + $('#z3').val();
+	      dt += '&product_name=' + $('#z4').val();
+
+	     url +=dt;
+
+             var xhr = new XMLHttpRequest();
+	     xhr.open('get', url, true);
+	     xhr.responseType = 'json';
+	     xhr.onload = function() {
+             	var status = xhr.status;
+		if (status == 200) {
+			alert(xhr.response.status);
+		} else {
+			alert(status);
+		}
+	     };
+	     xhr.send();
 	});
 }
